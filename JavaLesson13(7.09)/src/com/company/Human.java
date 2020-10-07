91be400c1e9fdb83e408ede0c73759757aad6159
tedGG <nazar05548@gmail.com>
2020-10-07 21:20:01 +0300

    homework about delegates

---
+++ JavaLesson13(7.09)/src/com/company/Human.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Human {
+    int weight;
+    int growth;
+
+
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getGrowth() {
+        return growth;
+    }
+
+    public void setGrowth(int growth) {
+        this.growth = growth;
+    }
+}
---
+++ JavaLesson13(7.09)/src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+
+import java.awt.*;
+
+public class Main  {
+
+
+    public static void main(String[] args) {
+            Fraction.showalldelegates();
+
+
+
+
+
+    }
+    public static void Menu(){
+        Verkhovnarada verkhovnarada = new Verkhovnarada();
+
+        switch (2) {
+            case 1:{
+                verkhovnarada.addFraction();
+                break;
+            }
+            case 2:{
+                verkhovnarada.deleteFraction();
+                break;
+            }
+            case 3:
+                verkhovnarada.showallfraction();
+                break;
+            case 4:
+                verkhovnarada.removeFraction();
+                break;
+            case 5:
+
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+        }
+    }
+
+
+}
